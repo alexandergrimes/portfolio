d546ec0c0a50b019b2f36773072927e82e3d4082
Alex Grimes <alex.s.grimes@gmail.com>
2019-05-01 17:30:08 -0600

    Nav menu closes on link click. Tooltip for email

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
+let navCats = document.querySelectorAll(".nav-cat");
+
 function transformHam(x) {
     x.classList.toggle("change");
-    let navCats = document.querySelectorAll(".nav-cat");
     for (let i of navCats) {
         i.classList.toggle("nav-cat-col");
     } 
+    let navCatCols = document.querySelectorAll(".nav-cat-col");
+    for (let n of navCatCols) {
+        n.addEventListener("click", function(c) {
+            x.classList.remove("change");
+            for (let items of navCatCols) {
+                items.classList.remove("nav-cat-col");
+            }
+        });
+    }
   }
+
+
